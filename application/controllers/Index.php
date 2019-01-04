eabdf7c9e14bda56024d5235a6e0a593c7fac471
tomosuko <tomosuko@192.168.179.4>
2019-01-05 00:25:47 +0900

    項目を修正

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,10 +1,17 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Index extends CI_Controller {
 
-	public function view()
-	{
-		$this->load->view('index');
-	}
+    public function view() {
+        $this->load->helper('html');
+        $this->load->helper('url');
+
+        $data['title'] = '';
+        $this->load->view('templates/header', $data);
+        $this->load->view('index');
+        $this->load->view('templates/footer');
+    }
 }
+
