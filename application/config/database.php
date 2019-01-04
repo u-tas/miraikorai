62a4d407a1233b1a774be73eab315776f26f095c
u-tas <u-tas@192.168.179.3>
2019-01-04 18:44:24 +0900

    DB情報を追記

--- application/config/database.php
+++ application/config/database.php
@@ -70,15 +70,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'local';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['local'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'root',
 	'database' => 'Miraikorai',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['remote'] = array(
+	'dsn'	=> '',
+	'hostname' => 'mysql630.db.sakura.ne.jp',
+	'username' => 'capricieux',
+	'password' => '1224swbt03',
+	'database' => 'capricieux_miraikorai',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
