9063036d7cb5c2bf93447b306c5a513db5f92b08
u-tas <u-tas@192.168.179.3>
2019-01-05 18:36:52 +0900

    コンストラクタでスーパークラスのコンストラクタを呼び出す処理を追加

--- application/models/Information_model.php
+++ application/models/Information_model.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Information_model extends CI_Model {
 
     public function __construct() {
+        parent::__construct();
         $this->load->database();
     }
 
