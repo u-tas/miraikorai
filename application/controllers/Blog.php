5f8432cdea25837ed5ba140ee03b9b5c45bded27
u-tas <u-tas@192.168.179.3>
2019-01-05 14:42:24 +0900

    Controller Blog.php,Stage.phpを修正し、各pageを表示 ※HTML,URL Helper呼び出し

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -2,11 +2,15 @@
 class Blog extends CI_Controller {
     public function view($kiji_no = NULL)
     {
+        $this->load->helper('html');
+        $this->load->helper('url');
+        
         if ( ! file_exists(APPPATH.'views/about/about.php'))
         {
             // おっと、そのページはありません！
             show_404();
         }
+        
         if ( $kiji_no === null) 
         {
             // 記事最新XX件の概要を取ってくる処理を入れる
--- application/controllers/Stage.php
+++ application/controllers/Stage.php
@@ -2,15 +2,18 @@
 class Stage extends CI_Controller {
     public function view()
     {
+        $this->load->helper('html');
+        $this->load->helper('url');
+        
         if ( ! file_exists(APPPATH.'views/about/about.php'))
         {
                 // おっと、そのページはありません！
                 show_404();
         }
 
-    $this->load->view('templates/header');
-    $this->load->view('stage/stage_list');
-    $this->load->view('templates/footer');
+        $this->load->view('templates/header');
+        $this->load->view('stage/stage_list');
+        $this->load->view('templates/footer');
     }
 }
 
