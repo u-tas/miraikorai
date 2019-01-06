2e0b88893133571811ed991c0a42c65df9091455
tomosuko <tomosuko@192.168.179.4>
2019-01-06 14:18:51 +0900

    blogのstyleを修正

--- application/views/blog/blog.php
+++ application/views/blog/blog.php
@@ -1,39 +1,12 @@
 <!DOCTYPE html>
 
-<html lang="ja">
-
 <head>
-
-    <title>未来コライ|miraikorai</title>
-
-
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="未来コライは、混声のバーバーショップを歌うカルテットグループです。">
-    <meta name="viewport" content="width=device-width,initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta name="format-detection" content="telephone=no" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/color_style.css" />
-    <link rel="stylesheet" href="css/header_footer.css" />
-    <link rel="stylesheet" href="css/blog_style.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!--web icon-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-    <script src="js/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(function(){
-            $("header").load("./header.html");
-            $(".footer_bottom").load("./footer.html");
-        })
-    </script>
+    <?= link_tag("css/blog_style.css")?>
 </head>
 
 <body>
     <div class="wrapper">
-        <header></header>
-
+        
         <div class="main_contents">
 
             <div class="title_contents bg_yellow">
@@ -94,8 +67,6 @@
     </div>
         </div>
         <!--main_contets-->
-
-        <div class="footer_bottom"></div>
     </div>
     <!--wrapper-->
 </body>
