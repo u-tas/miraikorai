bc199cda6daa98644ff70c2b022caa0e40645522
u-tas <u-tas@192.168.179.3>
2019-01-19 01:04:56 +0900

    周期的な色替えを実装

--- application/views/blog/blog_list.php
+++ application/views/blog/blog_list.php
@@ -1,6 +1,7 @@
 <head>
     <?= link_tag("css/list.css")?>
-    <script src="<?= base_url("js/jquery.min.js")?>"></script>
+
+    <script src="<?= base_url("js/list.js")?>"></script>
 </head>
 
 <body>
@@ -12,27 +13,34 @@
               <h1 class="pege_title">BLOG</h1>
           </div>
           <ul class="blog_list">
+            <?php $css_class = array
+                ( 0 => 'bg_red',
+                 1 => 'bg_blue',
+                 2 => 'bg_green',
+                 3 => 'bg_yellow'); 
+            ?>
+              <?php $count = 0;?>
               <?php foreach ($articles as $article) :?>
                 <li>
-                  <a href="<?= base_url("index.php/blog/".$article['id']) ?>"> <!--index.php-->
+                  <a href="<?= base_url("blog/".$article['id']) ?>"> <!--index.php-->
                 <div class="blog_cap">
                     <div class="blog_title"><?=$article['title']?></div>
                     <div class="member_part_bottom">
-                     <div class="member_part_box_left bg_red"></div>
-                     <div class="member_part_center bg_red"></div>
-                     <div class="member_part_box_right bg_red"></div>
+                     <div class="member_part_box_left <?=$css_class[$count%count($css_class)]?>"></div>
+                     <div class="member_part_center <?=$css_class[$count%count($css_class)]?>"></div>
+                     <div class="member_part_box_right <?=$css_class[$count%count($css_class)]?>"></div>
                     </div>
                     <p class="blog_date"><?= substr($article['publishment_date'], 0, 10) ?></p>
                 </div>
                 <div class="box_main gradation_1">
                 <div class="blog_image">
-                    <div class="overlay bg_red"></div>
+                    <div class="overlay <?=$css_class[$count%count($css_class)]?>"></div>
                     <?= img("img/blog/".$article['image_url'])?>
                 </div>
                 </div>
               </a>
                 </li>
-             <?php endforeach;?>
+             <?php $count++; endforeach;?>
             </ul>
         </div>
         <!--main_contets-->
