9640a016eda7d38667c8fc8881a8ca1261797b40
tomosuko <tomosuko@192.168.179.4>
2019-06-26 23:59:32 +0900

    galleryの更新

--- application/views/gallery/gallery.php
+++ application/views/gallery/gallery.php
@@ -1,5 +1,8 @@
 <head>
-    <?= link_tag("css/about_style.css")?>
+    <?= link_tag("css/gallery_style.css")?>
+    <?= link_tag("css/jquery.fs.boxer.css")?>
+    <?= link_tag("css/color.css")?>
+    <script src="../../../js/jquery.fs.boxer.min.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -16,10 +19,41 @@
                 <div class="title_sub_border_bottom bg_green">
                 </div>
             </div>
+            
+                <div class="backnumber">
+       <div class="backnumber_photo">
+           <ul>
+             <li><div class="video">
+                 <a href="https://www.youtube.com/embed/dVlTmWQ8_Tw" class="boxer_sample" data-gallery="videos">
+               <figure>
+                   <img src="images/gallery_images/blog_1.png" />
+                 <div class="overlay gradation_10"></div>
+                 <figcaption class="gradation_10">
+                   <h3>SEEMS LIKE OLD TIMES</h3>
+                 </figcaption>
+               </figure>
+                      
+                 </a></div> 
+                 </li>
+                 <li>
+                 <a href="https://www.youtube.com/embed/37jD6oLoQ_8" class="boxer_sample" data-gallery="videos">
+               <figure>
+                   <img src="images/gallery_images/blog_1.png" />
+                 <div class="overlay gradation_11"></div>
+                 <figcaption class="gradation_11">
+                   <h3>THE STARS AND STRIPES FOREVER</h3>
+                 </figcaption>
+               </figure>
+                 </a>
+                 </li>
+                 
+           </ul>
+       </div>
+       <script type="text/javascript">
+                jQuery(".boxer_sample").boxer();
+       </script>
 
-　　　　　　<div class="video">
-          <iframe src="https://www.youtube.com/embed/YzWB1AbXW94" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>  
+      </div>  
       </div>
         <!--main_contets-->
 
