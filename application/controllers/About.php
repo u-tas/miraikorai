cf98622312aa9755d85393c37b48c2dadf4e52d4
u-tas <u-tas@192.168.179.3>
2020-02-23 12:33:07 +0900

    Historyを削除

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -5,16 +5,13 @@ class About extends CI_Controller {
 
     public function __construct() 
     {
-        parent::__construct();
-        $this->load->model("History_model");        
+        parent::__construct();        
     }
     
     public function index()
     {        
         $data['title'] = 'about';        
         $this->load->view('templates/header',$data);
-        
-        $data['history'] = $this->History_model->get_history();
         $this->load->view('about/about',$data);
         $this->load->view('templates/footer');
     }
--- application/views/about/about.php
+++ application/views/about/about.php
@@ -309,39 +309,7 @@
 
 
 
-            <div class="title_sub_contents">
-                <h2 class="pege_title_sub red">History</h2>
-                <div class="title_sub_border_bottom bg_red">
-                </div>
-            </div>
 
-            <div class="about_history">
-                <div class="about_history_tree">
-                    <div class="box_top"></div>
-                    <div class="box_bottom"></div>
-                </div>
-                <div class="about_history_main">
- 
-                    <?php $year = '';?>
-                    <?php foreach ($history as $history_item) : ?>
-                        <?php if (strcmp($year, substr($history_item['publishment_date'], 0, 4))) : ?>
-                            <div class="about_history_year">
-                                <p><?=substr($history_item['publishment_date'], 0, 4)?></p>
-                            </div>
-                        <?php endif;?>
-                        <div class="about_history_event">
-                            <ul>
-                                <li class="event_title"><?=$history_item['title']?></li>
-                                <li class="event_date"><?=str_replace("-", ".", substr($history_item['publishment_date'], 5, 5))?></li>
-                            </ul>
-<!--                        <div class="event_text"></div>-->
-                    </div>
-                    <?php
-                        $year = substr($history_item['publishment_date'], 0, 4); 
-                        endforeach; ?>
-                </div>
-                <!--about_history_main -->
-            </div>
 
         </div>
         <!--main_contets-->
